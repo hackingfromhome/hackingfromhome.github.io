ce29ed9526ed480a9163278031fa864514e8aa35
carla <cfpcarla@gmail.com>
2020-04-11 16:18:16 -0400

    Implement Grid

--- src/home.jsx
+++ src/home.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper } from '@material-ui/core';
 
-const Navbar = () => {
-  return <Button color="primary">Olá Mundo!</Button>;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const Home = () => {
+  const classes = useStyles();
+  return (
+     <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+      </Grid>
+      < footer className={classes.footer} >
+        Sponsors
+      </footer >
+      </div>
+  );
 }
 
-export default Navbar;
+export default Home;
