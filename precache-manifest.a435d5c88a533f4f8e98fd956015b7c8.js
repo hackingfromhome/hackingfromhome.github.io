b3fa9149a07300f87337012a965676c8e4435133
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2020-04-14 19:31:06 -0400

    Updates

---
+++ precache-manifest.a435d5c88a533f4f8e98fd956015b7c8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c4c0e36f7c15bd7be78bc08a31dd88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c5569afb9adc1efe8a",
+    "url": "/static/css/main.bb711070.chunk.css"
+  },
+  {
+    "revision": "0ee21d1423894d3237f7",
+    "url": "/static/js/2.c134b54d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c134b54d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c5569afb9adc1efe8a",
+    "url": "/static/js/main.4e743137.chunk.js"
+  },
+  {
+    "revision": "4ae4fd69750ddd8abcdd",
+    "url": "/static/js/runtime-main.b9f45d43.js"
+  },
+  {
+    "revision": "b9bd28bf31b4e1ae57941afb9510d4b4",
+    "url": "/static/media/banner.b9bd28bf.png"
+  },
+  {
+    "revision": "378ac98354ca5a2a89dc55df1046e552",
+    "url": "/static/media/coinsquare_transparent.378ac983.png"
+  },
+  {
+    "revision": "fdcf9473e019fba136fc68cdf334d096",
+    "url": "/static/media/farinazzo.fdcf9473.png"
+  },
+  {
+    "revision": "b82b2709456c140377d359f4c2a4adcf",
+    "url": "/static/media/fran.b82b2709.png"
+  },
+  {
+    "revision": "6ec982b8b3313a14e6dbf3a4d0a44569",
+    "url": "/static/media/leo.6ec982b8.jpg"
+  },
+  {
+    "revision": "2470b612b0d78fb39bddca550361c8f7",
+    "url": "/static/media/luciano.2470b612.png"
+  }
+]);
